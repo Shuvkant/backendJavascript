e2b6bf2f051e8bb18626ff1d275e1a64437c2c27
Shuvkant <weird426344@gmail.com>
2024-10-28 21:40:32 +0545

    working with utils

--- src/index.js
+++ src/index.js
@@ -3,8 +3,21 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({
   path: "./.env",
 });
-connectDB();
+connectDB().then(() => {
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is running at port ${process.env.PORT}`);
+  });
+  app.get("/shuvkant", (req, res) => {
+    res.send("You are the hacker of the future generation");
+  });
+  app.get("/", (req, res) => {
+    res.send("Shuvkant Chaudhary Phanait");
+  });
+}).catch((err) => {
+  console.log("MONGO_DB CONNECTION FAILED !!! ", err);
+});
