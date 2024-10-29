5c24662724fc0046bffbc9009f3527cb66bd4b3a
Shuvkant <weird426344@gmail.com>
2024-10-29 18:43:11 +0545

    configuring postaman for backend

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,14 +6,14 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   const { fullName, email, password, username } = req.body;
-  console.log("email", email);
+  // console.log("email", email);
   if (
     [fullName, username, email, password].some((field) => {
       field?.trim() === "";
     })
   ) throw new ApiError(400, "All fields are required");
 
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{ username }, { email }],
   });
 
@@ -22,7 +22,15 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  let coverImageLocalPath;
+  if (
+    req.files && Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar file is required");
   }
@@ -51,9 +59,8 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(500, "Something went wrong while registering the user");
   }
 
-  return res.status(201).json(
-    new ApiResponse(200, createdUser, "User registered successfully"),
-  );
+  return res.status(201).json();
+  new ApiResponse(200, createdUser, "User registered successfully");
 });
 
 export { registerUser };
